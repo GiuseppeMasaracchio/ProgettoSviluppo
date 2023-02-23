cbe576fa8ffa09cf220349da66a33425f7af8016
Paolo Castellino <36105321+C5Arless@users.noreply.github.com>
2023-02-23 11:35:06 +0100

    Mouse Position fix

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour {
     public Vector2 Rotation = Vector2.zero;
+    
     public Quaternion CamRot(float horizontalSens) {
         //Devo riempire questa funzione per calcolare manualmente l'angolo di rotazione e restituirlo
         return Quaternion.Euler(Mathf.Clamp(Rotation.y * horizontalSens, -1000f, 2000f) * Time.fixedDeltaTime, Rotation.x * horizontalSens * Time.fixedDeltaTime, 0f);
@@ -11,5 +12,6 @@ public class CameraRotation : MonoBehaviour {
 
     public void xRot(Vector2 _xRotation) {
         this.Rotation = _xRotation;
+
     }
 }
