ed68ef992f71816114d92dddd82e47adfe64ed45
Paolo Castellino <36105321+C5Arless@users.noreply.github.com>
2023-02-23 14:10:33 +0100

    Scripts/var/methods renaming

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraRotation : MonoBehaviour {
-    public Vector2 Rotation = Vector2.zero;
+    public Vector2 screenPosition = Vector2.zero;
     
     public Quaternion CamRot(float horizontalSens) {
-        //Devo riempire questa funzione per calcolare manualmente l'angolo di rotazione e restituirlo
-        return Quaternion.Euler(Mathf.Clamp(Rotation.y * horizontalSens, -1000f, 2000f) * Time.fixedDeltaTime, Rotation.x * horizontalSens * Time.fixedDeltaTime, 0f);
+        return Quaternion.Euler(Mathf.Clamp(screenPosition.y * horizontalSens, -1000f, 2000f) * Time.fixedDeltaTime, screenPosition.x * horizontalSens * Time.fixedDeltaTime, 0f);
     }
 
-    public void xRot(Vector2 _xRotation) {
-        this.Rotation = _xRotation;
+    public void ScreenPosition(Vector2 mousePosition) {
+        this.screenPosition = mousePosition;
 
     }
 }
--- Assets/Scripts/PlayerMov.cs
+++ Assets/Scripts/PlayerMov.cs
@@ -7,13 +7,9 @@ public class PlayerMov : MonoBehaviour {
     [SerializeField] Transform camHolder;
     [SerializeField] float horizontalSens;
     [SerializeField] Rigidbody player;
-    //[SerializeField] Camera sCam;
-    //[SerializeField] PlayerInput puzzo;
 
     public CameraRotation cam;
     public Movement move;
-
-    // Start is called before the first frame update
 
     void Awake() {
         cam = camHolder.GetComponent<CameraRotation>();
@@ -24,25 +20,11 @@ public class PlayerMov : MonoBehaviour {
     void Start() {
         Cursor.lockState = CursorLockMode.Confined;
         
-        //camHolder.transform.rotation.Set(0f, 0f, 0f, 0f);
-        
     }
 
-    // Update is called once per frame
     void Update() {
-        //Debug.Log(Mouse.current.position.ReadValue());
-        /*
-        if (Keyboard.current.escapeKey.wasPressedThisFrame) {
-            puzzo.SwitchCurrentActionMap("UI");
-            Debug.Log(puzzo.currentActionMap.name);
-
-        }
-        */
-        //Debug.Log(camHolder.rotation);
-        //Debug.Log(camHolder.localRotation);
-        
         camHolder.rotation = cam.CamRot(horizontalSens);
-        player.AddForce(move.Mov().normalized * 500f * Time.fixedDeltaTime);
+        player.AddForce(move.Move().normalized * 500f * Time.fixedDeltaTime);
         
     }
 }
