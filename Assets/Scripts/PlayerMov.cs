22b95d1de76b88f712c42c00215f2fa83e2b2479
Paolo Castellino <36105321+C5Arless@users.noreply.github.com>
2023-02-24 15:00:32 +0100

    Movement.cs Updated ModulesTemplate.cs Added

--- Assets/Scripts/PlayerMov.cs
+++ Assets/Scripts/PlayerMov.cs
@@ -4,27 +4,22 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMov : MonoBehaviour {
-    [SerializeField] Transform camHolder;
+
     [SerializeField] float horizontalSens;
-    [SerializeField] Rigidbody player;
 
     public CameraRotation cam;
     public Movement move;
 
     void Awake() {
-        cam = camHolder.GetComponent<CameraRotation>();
-        move = player.GetComponent<Movement>();
 
     }
 
     void Start() {
-        Cursor.lockState = CursorLockMode.Confined;
         
     }
 
     void Update() {
-        camHolder.rotation = cam.CamRot(horizontalSens);
-        player.AddForce(move.Move().normalized * 500f * Time.fixedDeltaTime);
+        //move.Walk();
         
     }
 }
