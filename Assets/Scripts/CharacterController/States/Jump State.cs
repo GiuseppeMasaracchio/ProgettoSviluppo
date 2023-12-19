351cd7cdeaec103653c2c3293baac92b0060c6fa
Paolo Castellino <36105321+C5Arless@users.noreply.github.com>
2023-12-19 20:15:42 +0100

    Warp prefab (No triggers) Shader error fix + Custom Jump clip (OG based)

--- Assets/Scripts/CharacterController/States/Jump State.cs
+++ Assets/Scripts/CharacterController/States/Jump State.cs
@@ -71,8 +71,9 @@ public class JumpState : BaseState, IContextInit, IWalk {
         Ctx.IsIdle = false;
     }
     public void InitializeParticle() {
+        Ctx.Vfx.GetComponent<VisualEffect>().Stop();
         Vector3 offset = Ctx.Asset.transform.position + (new Vector3(0f, -.75f, 0f));
-        Ctx.Vfx.transform.position = offset - (new Vector3(0f, Ctx.Vfx.transform.position.y/2, 0f));
+        Ctx.Vfx.transform.position = offset - (new Vector3(0f, Ctx.Vfx.transform.localPosition.y/2, 0f));
         Ctx.Vfx.GetComponent<VisualEffect>().Play();
     }
     private void HandleJump(Rigidbody rb) {
