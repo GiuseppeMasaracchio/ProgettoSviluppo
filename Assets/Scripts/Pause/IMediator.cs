51608df09c964b9c11d792aa6c8f082806ab8d80
Giuseppe Masaracchio <Masaracchio38@gmail.com>
2024-01-15 22:58:22 +0100

    Aggiunta ModalWindowUI

---
+++ Assets/Scripts/Pause/IMediator.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMediator {
+    public void NotifyMediator(GameObject sender, string buttonName); //true se a buon fine
+
+}
