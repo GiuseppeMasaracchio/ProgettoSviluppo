450ca3f028fae281993cbca2c1730aefefa9101f
Paolo Castellino <36105321+C5Arless@users.noreply.github.com>
2024-05-18 15:16:41 +0200

    Pivot rotation fix (Time.deltaTime added to rotation) + Scenes tweaks (Warp 1 - 2) + Animator controllers tweaks + Editor Settings update (Domain reload on Play)

--- Assets/Scripts/ObjectBehaviour/PivotRotation/PivotRotation.cs
+++ Assets/Scripts/ObjectBehaviour/PivotRotation/PivotRotation.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class PivotRotation : MonoBehaviour {
     [SerializeField]
-    [Range(0.01f, 2f)] float speed;
+    [Range(10f, 100f)] float speed;
     void Update() {
-        this.transform.Rotate(Vector3.up * speed);
+        this.transform.Rotate(Vector3.up * speed * Time.deltaTime);
     }
 }
