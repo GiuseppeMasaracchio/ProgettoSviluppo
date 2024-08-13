23fb09a053e91d1c86aea6289ad248faf349c3a1
Paolo Castellino <36105321+C5Arless@users.noreply.github.com>
2024-08-13 21:45:14 +0200

    Companion Controller update (now has all variables)

--- Assets/Scripts/Companion/Context/CompanionController.cs
+++ Assets/Scripts/Companion/Context/CompanionController.cs
@@ -4,6 +4,25 @@ using UnityEngine.VFX;
 public class CompanionController : MonoBehaviour {
     [SerializeField] PlayerInfo _playerInfo;
     [SerializeField] VisualEffect _spark;
+    [SerializeField] Collider _stuckCollider;
+
+    [SerializeField]
+    [Range(0f, 5f)] float horizontalOffset;
+
+    [SerializeField]
+    [Range(0f, 5f)] float verticalOffset;
+
+    [SerializeField]
+    [Range(0f, 10f)] float maxVelocity;
+
+    [SerializeField]
+    [Range(0f, 10f)] float limitDistance;
+    
+    private CBaseState _currentRootState;
+    private CBaseState _currentSubState;
+    private CompanionStateHandler _stateHandler;
+
+    private GameObject _playerHead;
 
     private bool isStuck = false;
     private bool isOperative = false;
@@ -14,15 +33,21 @@ public class CompanionController : MonoBehaviour {
     private bool isFocusing = false;
     private bool isUnstucking = false;
 
-    private GameObject _playerHead;
+    private float limitDistanceMax;
+    private Vector3[] targetPosition = new Vector3[4];
+    private Vector3 lockedPosition;
 
-    private CBaseState _currentRootState;
-    private CBaseState _currentSubState;
-    private CompanionStateHandler _stateHandler;
+    private bool targetLocked = false;
+
+    private float velocity;
+    private float playerDistance;
 
     public CBaseState CurrentRootState { get { return _currentRootState; } set { _currentRootState = value; } }
     public CBaseState CurrentSubState { get { return _currentSubState; } set { _currentSubState = value; } }
     public CompanionStateHandler StateHandler { get { return _stateHandler; } set { _stateHandler = value; } }
+
+    public GameObject PlayerHead { get { return _playerHead; } set { _playerHead = value; } }
+    public Collider StuckCollider { get { return _stuckCollider; } set { _stuckCollider = value; } }
 
     public bool IsStuck { get { return isStuck; } set { isStuck = value; } }
     public bool IsOperative { get { return isOperative; } set { isOperative = value; } }
@@ -32,9 +57,22 @@ public class CompanionController : MonoBehaviour {
     public bool IsFocusing { get { return isFocusing; } set { isFocusing = value; } }
     public bool IsUnstucking { get { return isUnstucking; } set { isUnstucking = value; } }
 
+    public float HorizontalOffset { get { return horizontalOffset; } }
+    public float VerticalOffset { get { return verticalOffset; } }
+    public float MaxVelocity { get { return maxVelocity; } }
+    public float LimitDistance { get { return limitDistance; } }
+    public float LimitDistanceMax { get { return limitDistanceMax; } set { limitDistanceMax = value; } }
+    public Vector3[] TargetPositions { get { return targetPosition; } set { targetPosition = value; } }
+    public bool TargetLocked { get { return targetLocked; } set { targetLocked = value; } }
+    public Vector3 LockedPositions { get { return lockedPosition; } set { lockedPosition = value; } }
+    public float Velocity { get { return velocity; } set { velocity = value; } }
+    public float PlayerDistance { get { return playerDistance; } set { playerDistance = value; } }
+
     void Awake() {
         _stateHandler = new CompanionStateHandler(this);
         _playerHead = GameObject.Find("PlayerHead");
+
+        limitDistanceMax = limitDistance + horizontalOffset;
 
         /*
         _currentRootState = CompanionStateHandler.Operative();
