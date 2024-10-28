3d22f3512186692a9752553811b8ca2f1bbd802c
Giuseppe Masaracchio <Masaracchio38@gmail.com>
2024-10-28 15:55:30 +0100

    Canvas template Canvas MainMenu - inizio

--- Assets/Scripts/MainMenu/CanvasScript.cs
+++ Assets/Scripts/MainMenu/CanvasScript.cs
@@ -1,17 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.UIElements;
-
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 public class CanvasScript : MonoBehaviour
 {
     [SerializeField] private AudioMixer _mixer;
+    [SerializeField] private Button _currentTab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(_currentTab != null)     _currentTab.Select();
     }
 
     // Update is called once per frame
@@ -19,6 +18,27 @@ public class CanvasScript : MonoBehaviour
     {
         
     }
+
+    public void OnTabPressed() {
+        Button _tab = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
+        
+        if (_currentTab == _tab) return;
+        _currentTab.interactable = true;
+        EventSystem.current.SetSelectedGameObject(null);
+
+        _tab.Select();
+        _tab.interactable = false;
+        _currentTab = _tab;
+    }
+
+    public void OnContinuePress() {
+        //Load last active scene
+    }
+
+    public void OnQuitPress() {
+        Application.Quit();
+    }
+
     public void SetMasterVolume(float val) {
         _mixer.SetFloat("Master", val);
     }
