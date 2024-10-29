79c9eb7dea580e3996988250d80eaa91268a04e7
Giuseppe Masaracchio <Masaracchio38@gmail.com>
2024-10-29 14:33:07 +0100

    OptionsContainer con scrollbar

--- Assets/Scripts/MainMenu/CanvasScript.cs
+++ Assets/Scripts/MainMenu/CanvasScript.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.EventSystems;
@@ -7,10 +10,15 @@ public class CanvasScript : MonoBehaviour
     [SerializeField] private AudioMixer _mixer;
     [SerializeField] private Button _currentTab;
 
+    [SerializeField] private GameObject _audioSettings;
+    [SerializeField] private GameObject _videoSettings;
+
+
     // Start is called before the first frame update
     void Start()
     {
         if(_currentTab != null)     _currentTab.Select();
+        
     }
 
     // Update is called once per frame
@@ -31,6 +39,32 @@ public class CanvasScript : MonoBehaviour
         _currentTab = _tab;
     }
 
+    private void InitAudioSettings() {
+        Slider[] _sliders = _audioSettings.GetComponentsInChildren<Slider>();
+        float x;
+
+        _mixer.GetFloat("MasterVolume", out x);
+        _sliders[0].value = x;
+
+        _mixer.GetFloat("MusicVolume", out x);
+        _sliders[1].value = x;
+
+        _mixer.GetFloat("EffectsVolume", out x);
+        _sliders[2].value = x;
+    }
+
+    private void InitVideoSettings() {
+        TMP_Dropdown[] _dropdowns = _videoSettings.GetComponentsInChildren<TMP_Dropdown>();
+        List<String> tempList = new List<String>();
+
+        foreach(Resolution r in Screen.resolutions) {
+            tempList.Add(r.width + "x" + r.height);
+        }
+        _dropdowns[0].AddOptions(tempList);
+
+
+    }
+
     public void OnContinuePress() {
         //Load last active scene
     }
@@ -48,4 +82,5 @@ public class CanvasScript : MonoBehaviour
     public void SetEffectsVolume(float val) {
         _mixer.SetFloat("Effects", val);
     }
+
 }
