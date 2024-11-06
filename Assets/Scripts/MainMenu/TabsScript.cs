61023732113efe04f225b92225d98dadb74d8810
Giuseppe Masaracchio <Masaracchio38@gmail.com>
2024-11-06 23:35:54 +0100

    Miglioramento TabsScript

--- Assets/Scripts/MainMenu/TabsScript.cs
+++ Assets/Scripts/MainMenu/TabsScript.cs
@@ -5,17 +5,12 @@ using UnityEngine.UI;
 
 public class TabsScript : MonoBehaviour
 {
-    //[SerializeField] private GameObject _tabsBox;
-    private Button[] _tabs;
     private Button _currentTab;
-    [SerializeField] private GameObject _videoSettingsBox;
-    [SerializeField] private GameObject _audioSettingsBox;
-    [SerializeField] private float animTime = 0.2f;
+    private GameObject _currentBox;
 
     // Start is called before the first frame update
     void Start()
     {
-        _tabs = GetComponentsInChildren<Button>();
     }
 
     // Update is called once per frame
@@ -26,47 +21,24 @@ public class TabsScript : MonoBehaviour
 
 
 
-    private void TabsBehaviour() {
-        Button temp = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
-        if (_currentTab == temp) return;
-
-        _currentTab.interactable = true;
-        temp.interactable = false;
-        _currentTab = temp;
-    }
 
     //Metodi da usare altrove
 
-    private void BoxSwitch() {
+    public void BoxSwitch(GameObject newBox) {
+        if (_currentBox == newBox) return;
 
+        _currentBox.SetActive(false);
+        newBox.SetActive(true);
+        _currentBox = newBox;
     }
 
-    private IEnumerator BoxFadeIn(GameObject box) {
-        float x = box.transform.localScale.x;
-        float y = box.transform.localScale.y;
+    public void TabSelect(Button newTab) {
+        if (_currentTab == newTab) return;
 
-        box.SetActive(true);
-
-        box.transform.localScale.Set(
-            Mathf.Lerp(0f, x, animTime),
-            Mathf.Lerp(0f, y, animTime),
-            0f
-            ) ;
-
-        yield return new WaitForSeconds(animTime);
-    }
-    private IEnumerator BoxFadeOut(GameObject box) {
-        float x = box.transform.localScale.x;
-        float y = box.transform.localScale.y;
-
-        box.transform.localScale.Set(
-            Mathf.Lerp(x, 0f, animTime),
-            Mathf.Lerp(y, 0f, animTime),
-            0f
-            );
-
-        yield return new WaitForSeconds(animTime);
-        box.SetActive(false);
-        box.transform.localScale.Set(x, y, 0f);
+        _currentTab.Select();
+        _currentTab.interactable = true;
+        newTab.Select();
+        newTab.interactable = false;
+        _currentTab = newTab;
     }
 }
